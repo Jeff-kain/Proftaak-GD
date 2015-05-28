bfc2ff3b214653fa6fda9f4b2b2a4352998d43b0
TimOomis <t.oomis@student.fontys.nl>
2015-05-28 13:32:25 +0200

    AddedItems
    Basic version of item attributed, will be changed later with error
    checks and/or constraints.

---
+++ Crescape/Assets/Scripts/Item.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	public bool Elastic;
+	public bool Floating; 
+	public bool JumpPower; 
+	public bool IsPlatform;
+	public bool Proportions;
+	public bool Sticky; 
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void SetSticky(Item I)
+	{
+		I.Sticky = true;
+	}
+
+	public void SetFloating(Item I)
+	{
+		I.Floating = true;
+	}
+
+	public void SetElastic(Item I)
+	{
+		I.Elastic = true;
+	}
+
+}
